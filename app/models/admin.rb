069ddaec611fabf73f4ecbb000853e8d6bbfffbb
Artur Melo <32469212+Vandrulfr@users.noreply.github.com>
2020-02-06 16:56:55 -0300

    views redundantes removidas

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,28 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
+  attr_accessor :login
+
+  # "getter"
+  # def login
+  #   @login
+  # end
+
+  # "setter"
+  # def login=(str)
+  #  @login = str
+  # end
+
+  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ['lower(matricula) = :value ',
+       { value: login.strip.downcase }]
+    ).first
+  end
 end
