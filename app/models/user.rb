db2b53d5f1df4d076c1784f4b217e35f86f1db51
Artur Melo <arturmelo@id.uff.br>
2020-02-20 17:24:45 -0300

    exibe professor atual no menu de professores

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ApplicationRecord
   scope :disponiveis, lambda { |turma|
     left_outer_joins(:turmas).where(role: 'professor').merge(Turma.disponiveis(
       turma.inicio, turma.fim, turma.dias
-    ).or(where('turmas.id is NULL')))
+    ).or(where('turmas.id is NULL')).or(where(id: turma.user_id)))
   }
 
   private
