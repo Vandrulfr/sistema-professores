84286412d589214cb0e1a456461b6359c0bc90dc
vandrulfr <arturmelo@id.uff.br>
2020-02-21 21:08:03 -0300

    Teste de adição de professor à turma

--- spec/feature/turma_spec.rb
+++ spec/feature/turma_spec.rb
@@ -7,5 +7,15 @@ RSpec.feature 'Criar turma', type: :feature, js: true do
     cria_turma
     expect(page).to have_text('Materia')
   end
+  scenario 'Secretario atribui professor a turma' do
+    FactoryBot.rewind_sequences
+    create(:user, :sec)
+    create(:user, :professor)
+    cria_turma
+    click_link('Adicionar')
+    find('#p').click
+    click_button('Salvar')
+    expect(page).to have_text('John 2 Doe')
+  end
 end
 
